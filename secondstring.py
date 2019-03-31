e632530a864cc6acafa94c142e88f51dc45d9d7e
Cóbhan Phillipson <cobhan.phillipson@gmail.com>
2019-03-31 23:54:52 +0100

    Q6

---
+++ secondstring.py
@@ -0,0 +1,11 @@
+# 6. Write a program that takes a user input string and outputs every second word.
+
+n = str(input("Please enter a sentence: "))
+
+print (n.split()[0])
+print (n.split()[2])
+print (n.split()[4])
+print (n.split()[6])
+
+
+
