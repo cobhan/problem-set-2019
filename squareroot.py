c4ae470d17c67bd444c4328fd21e97d988162f85
Cóbhan Phillipson <cobhan.phillipson@gmail.com>
2019-04-01 20:55:16 +0100

    Q7

---
+++ squareroot.py
@@ -0,0 +1,6 @@
+#7 Write a program that that takes a positive floating point number as input and outputs an approximation of its square root.
+
+n = int(input("Please enter a positive number: ")) #user enters positive number
+print(n**0.5) #the basic laws of exponents state that n to the power of 0.5 = n to the power of n
+
+# http://mathforum.org/library/drmath/view/65402.html
